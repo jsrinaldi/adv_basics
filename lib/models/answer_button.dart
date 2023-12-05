1ce6b3c945fd4942a97a02feee67744751f59e4c
Jaime Rinaldi <jsrinaldi@gmail.com>
2023-12-05 12:35:44 +0100

    C05122023-001
    before class 71 use of spread operator ...

--- lib/models/answer_button.dart
+++ lib/models/answer_button.dart
@@ -10,7 +10,11 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
-      style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40,),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
         backgroundColor: const Color.fromARGB(255, 33, 1, 95),
         foregroundColor: Colors.white,
         shape: RoundedRectangleBorder(
--- lib/models/questions_screen.dart
+++ lib/models/questions_screen.dart
@@ -1,5 +1,6 @@
-import 'package:adv_basics/models/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/models/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,15 +12,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text('QuestionsScreen', style: TextStyle(color: Colors.white),),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
           const SizedBox(height: 31),
-          AnswerButton('Answer 1', () { }),
-          AnswerButton('Answer 2', () { }),
-          AnswerButton('Answer 3', () { }),
+          ...currentQuestion.answers.map(
+            (answer) {
+              return AnswerButton(answer, () {});
+            },
+          ),
         ],
       ),
     );
