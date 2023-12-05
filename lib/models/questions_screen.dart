dff85f2b3dfbfcf09d229423a2c71b72eacf0458
Jaime Rinaldi <jsrinaldi@gmail.com>
2023-12-05 16:52:21 +0100

    C05122023-002
    before class 72

--- lib/models/questions_screen.dart
+++ lib/models/questions_screen.dart
@@ -14,22 +14,27 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(BuildContext context) {
     final currentQuestion = questions[0];
     return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(height: 31),
-          ...currentQuestion.answers.map(
-            (answer) {
-              return AnswerButton(answer, () {});
-            },
-          ),
-        ],
+            const SizedBox(height: 31),
+            ...currentQuestion.answers.map(
+              (answer) {
+                return AnswerButton(answer, () {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
